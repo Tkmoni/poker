1a81822335e81c02c45cfb5567f22eb67adcc7c6
nemo <wangao5230@163.com>
2016-07-06 16:30:20 +0800

    test1

---
+++ poker/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using poker.rules;
+
+namespace poker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void StartGame(object sender, RoutedEventArgs e)
+        {
+
+            card_type game = new card_type();
+            int[] pile = new int[52];
+            pile = game.newcardlist();
+            int[,] PlayerCard=new int[10,2];
+            int Nplayer = Int32.Parse(playerNB.Text);
+            int Lplayer = Int32.Parse(playerLoc.Text);
+            int CardNow = 0;
+            for (int a=0; Nplayer>a;a++,CardNow++)
+            {
+                PlayerCard[a,0] = pile[CardNow];
+                CardNow++;
+                PlayerCard[a,1] = pile[CardNow];
+            }
+            ShowCardOnTable(PlayerCard,Nplayer, Lplayer);
+        }
+
+        public void ShowCardOnTable(int[,] Playercard, int Nplayer, int Lplayer)
+        {
+            for (int a=0; Nplayer>a; a++)
+            {
+                if (a != Lplayer)
+                    Console.WriteLine("player" + a);
+                else
+                    Console.WriteLine("you");
+                Console.WriteLine(Playercard[a, 0] + "♣" + Playercard[a, 1]);
+            }
+        }
+    }
+}
---
+++ poker/rules/card_type.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace poker.rules
+{
+    class card_type
+    {
+        
+        
+
+        public card_type()
+        {
+            int[] card = new int[52];
+            string[] cardtype = { "Diamond", "Heart", "Spade", "Club" };
+            int[] cardlist = newcardlist();
+        }
+
+
+        public int[] newcardlist()
+        {
+            int[] card = new int[52];            
+            int[] list = new int[52];
+            int id;
+            int site = 52;
+            Random a = new  Random();
+
+            for (int x=0; x<52; x++)
+            {
+                card[x] = x;
+                Console.WriteLine(card[x]);
+            }
+
+            for (int y=0; y<52; y++)
+            {
+                id = a.Next(0, site-1);
+                list[y] = card[id];
+                site--;
+                card[id] = card[site];
+                
+            }
+
+            
+            return list;
+        }
+        public void Showlist()
+        {
+
+        }
+
+    }
+}
